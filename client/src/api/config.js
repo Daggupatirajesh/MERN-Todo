08969e9c8d5f2bc10caa2cc52508e6a16edf6b3c
Daggupatirajesh <drajesh0307@gmail.com>
2023-10-02 17:02:09 +0530

    first commit

---
+++ client/src/api/config.js
@@ -0,0 +1,2 @@
+export const API_URL=`http://localhost:3001`
+export const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NjE4MDE2N30.xKRCUP_dG3LyZS_lkhcCStGH6iCJFh4B-M8dz5XNH5Q'
